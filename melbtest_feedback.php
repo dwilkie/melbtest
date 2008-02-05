60f18739a97de8049ead86aaf688e762c5812083
dave <dave@0553fc4d-6539-405c-8d44-5d98ecb26157>
2008-02-05 02:51:39 +0000

    finalised email message and added validation for feedback content and comments section. Also moved dynamic content into single php block - UNTESTED

--- melbtest_feedback.php
+++ melbtest_feedback.php
@@ -15,11 +15,11 @@
   Version 0.2; 2008-01-31 Fixed validation of email and company, updated div and class tags for styling,  modified feedback form to remember choices on invalid submissions, integrated emailing of results,
                                              added comments, added all feedback criteria and added other comments text area. - DCW
   Version 0.3; 2008-02-02 Initialised all variables and simplyfied code for displaying criteria into a loop. - DCW
-  Version 0.4; 2008-02-04 Modified criterion and rating numbers to make them more user readable, added comments to feedback form creation, added new-lines to php outputs for readability and started modifying email content. - DCW
+  Version 0.4; 2008-02-04 Modified criterion and rating numbers to make them more user readable, added comments to feedback form creation,
+                                              added new-lines to php outputs for readability and started modifying email content. - DCW
+  Version 0.5; 2008-02-05 Added validation to ensure all feedback criteria is rated, finished email content and moved dynamic data into one php block. - DCW
 
   todo:
-    Add validation to ensure all criteria are rated.
-    Finish email content
 -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
@@ -30,19 +30,37 @@
   <body>
     <div class ="content">
       <h1>Melbourne Testing Services Feedback Form</h1>
+      <!--start dynamic content-->
       <?php
-        function validate_string($rString)
-        {
-          $pattern = "^[^\<\>]+$";
-          return ereg($pattern, $rString);
-        }
-
+        // criteria for feedback (add more here)
+        $criteria[0] = "Friendiness of staff";
+        $criteria[1] = "Appropriate knowledge of staff on initial enquiry";
+        $criteria[2] = "Efficient handling of testing request";
+        $criteria[3] = "Professionalism";
+        $criteria[4] = "Efficiency of turnaround time";
+        $criteria[5] = "Reporting";
+        $criteria[6] = "Appropriate explanation of test results";
+        $criteria[7] = "After testing communication";
+        
+        // rating choices for feedback (add more here)
+        $ratings[0] = "N/A";
+        $ratings[1] = "Low";
+        $ratings[2] = "Medium";
+        $ratings[3] = "High";
+        
         function validate_name($rString)
         {
           $mod_string = str_replace(' ', '', $rString);
           $pattern = "^[a-zA-Z]+$";
           return ereg($pattern, $mod_string);
         }
+        
+        function validate_string($rString)
+        {
+          $mod_string = str_replace(' ', '', $rString);
+          $pattern = "^[^<>]+$";
+          return ereg($pattern, $mod_string);
+        }        
 
         function validate_email($rString)
         {
@@ -50,15 +68,15 @@
           return ereg($pattern, $rString);
         }
 
-        function valid_string_text()
-        {
-          return 'Cannot contain &lt; or &gt;';
-        }
-
         function valid_name_text()
         {
-          return 'Must only contain letters from A-Z';
+          return 'Can only contain letters from A-Z';
         }
+        
+        function valid_string_text()
+        {
+          return 'Must not contain angle brackets (&lt; &gt;)';
+        }        
         
         if (isset($_POST['submit_form']))
         {
@@ -85,6 +103,23 @@
               $error = true;
               $error_text = $error_text."\r\n".'<li>Email not valid</li>';
             }
+            
+            if (!validate_string($_POST['other_comments']))
+            {
+              $error = true;
+              $error_text = $error_text."\r\n".'<li>Comments contain invalid characters - '.valid_string_text().'</li>';
+            }
+            
+            $criterion_nr = 1;
+            foreach ($criteria as $criterion)
+            {
+              if (!isset($_POST["criteria_".$criterion_nr]))
+              {
+                $error = true;
+                $error_text = $error_text."\r\n".'<li>Feedback item #'.$criterion_nr.' ('.$criterion.') not completed</li>';
+              }
+            }
+            
             if($error)
             {
               echo '<div class="errors">'."\r\n".'<p>'."\r\n".'Could not submit form. Please fix the following errors:'."\r\n".'</p>'."\r\n".'<ol>'."\r\n".substr($error_text, 2).'</ol>'."\r\n".'</div>';
@@ -93,7 +128,7 @@
             {
               echo '<p class="success">Successfully submitted form.</p>';
 
-              // Change email destination options here
+              // change email destination options here
               $feedback_destination = 'dwilkie@gmail.com';
               $recipient_first_name = 'Leanne';
               $recipient_surname = 'Wilkie';
@@ -101,7 +136,7 @@
               // subject
               $subject = 'Feedback from MTS website';
 
-              // message
+              // start of message
               $message = '
               <html>
                 <head>
@@ -109,15 +144,49 @@
                 </head>
                 <body>
                   <p>
-                    Hi '.$recipient_first_name.',<br />
-                    I have left feedback on your site. The details are as follows: <br />
-                    My Name: '.$_POST['contact_name'].'<br />
+                    Hi '.$recipient_first_name.',<br /><br />
+                    I have left feedback on your site. The details are as follows: <br />';
+
+                    // contact name
+                    $message .= '<strong>My name:</strong>&nbsp;'.$_POST['contact_name'].'<br />';
+                    
+                    if (!empty($_POST['company']))
+                    {
+                      //company (if entered)
+                      $message .= '<strong>Company:</strong>&nbsp;'.$_POST['company'].'<br />';
+                    }
+                    //email
+                    $message .= '<strong>My email:</strong>&nbsp;<a href="mailto:'.$_POST['email'].'?subject=RE: '.$subject.'">'.$_POST['email'].'</a><br /><br />';
+                    
+                    //feedback
+                    $message .= '<strong>My feedback:</strong><br /><br />';
+                    
+                    $criterion_nr = 1;
+                    foreach ($criteria as $criterion)
+                    {
+                      {
+                        $rating_index = $_POST["criteria_".$criterion_nr]-1;
+                        $message .= '<em>'.$criterion_nr.'.&nbsp;'.$criterion.':</em>&nbsp;<strong>'.$ratings[$rating_index].'</strong><br />';
+                      }
+                    }
+                    
+                    //other comments
+                    if (!empty($_POST['other_comments']))
+                    {
+                      $message .= '<br /><strong>Other comments:</strong><br />';
+                      $message .= $_POST['other_comments'];
+                    }
+                    
+                    //end of message
+                    $message .= '
+                    <br /><br />
+                    Kind regards,<br /><br />'.$_POST['contact_name'].'
                   </p>
                 </body>
               </html>
               ';
 
-              // To send HTML mail, the Content-type header must be set
+              // to send html mail, the content-type header must be set
               $headers  = 'MIME-Version: 1.0'."\r\n";
               $headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
               $headers .= 'To: '.$recipient_first_name.' '.$recipient_surname.' <'.$feedback_destination.'>'."\r\n";
@@ -127,132 +196,125 @@
             }
           }
         }
-      ?>
-      <form class="feedback" action=<?php echo '"'.$_SERVER['PHP_SELF'].'"'; ?> method="post">
-        <fieldset class = "main">
-          <legend>MTS Feedback Form</legend>
-            <h2>Customer Details</h2>
-            <p class= "customer_details">
-              <label class = "text" for = "contact_name">*Contact Name:</label>
-              <?php
-                if (isset($_POST["contact_name"]))
-                {
-                  echo '<input type = "text" id="contact_name" name="contact_name" value = "'.$_POST["contact_name"].'" />'."\r\n";
-                }
-                else
-                {
-                  echo '<input type = "text" id="contact_name" name="contact_name" />'."\r\n";
-                }
-              ?>
-              <br />
-              <label class = "text" for = "company">Company:</label>
-              <?php
-                if (isset($_POST["company"]))
-                {
-                  echo '<input type = "text" id="company" name="company" value = "'.$_POST["company"].'" />'."\r\n";
-                }
-                else
-                {
-                  echo '<input type = "text" id="company" name="company" />'."\r\n";
-                }
-              ?>
-              <br />
-              <label class = "text" for = "email">*Email:</label>
-              <?php
-                if (isset($_POST["email"]))
-                {
-                  echo '<input type = "text" id="email" name="email" value = "'.$_POST["email"].'" />'."\r\n";
-                }
-                else
-                {
-                  echo '<input type = "text" id="email" name="email" />'."\r\n";
-                }
-              ?>
-            </p>
-            <h2>Feedback</h2>
-            <p class = "feedback_criteria">
-              <?php
-                $criterion_nr = 1;
-                
-                // Criteria for feedback (add more here)
-                $criteria[0] = "Friendiness of staff";
-                $criteria[1] = "Appropriate knowledge of staff on initial enquiry";
-                $criteria[2] = "Efficient handling of testing request";
-                $criteria[3] = "Professionalism";
-                $criteria[4] = "Efficiency of turnaround time";
-                $criteria[5] = "Reporting";
-                $criteria[6] = "Appropriate explanation of test results";
-                $criteria[7] = "After testing communication";
-                
-                // Rating choices for feedback (add more here)
-                $ratings[0] = "N/A";
-                $ratings[1] = "Low";
-                $ratings[2] = "Medium";
-                $ratings[3] = "High";
-                
-                foreach ($criteria as $criterion)
-                {
-                  // output a comment e.g. <!-- Criterion 1 (Friendliness of staff) -->
-                  echo '<!--Criterion '.$criterion_nr.' ('.$criterion.')-->'."\r\n";
-                  
-                  // output the actual criteria followed by a colon (:)
-                  echo $criterion.': <br />'."\r\n";
-                  
-                  $rating_nr = 1;
+        //start form
+        echo '<form class="feedback" action="'.$_SERVER['PHP_SELF'].'" method="post">'."\r\n";
+          //form border
+          echo '<fieldset class = "main">'."\r\n";
+            //legend
+            echo '<legend>MTS Feedback Form</legend>'."\r\n";
 
-                  foreach ($ratings as $rating)
-                  {
-                    // output a comment e.g. <!-- Rating 1 (N/A) -->
-                    echo '<!--Rating '.$rating_nr.' ('.$rating.')-->'."\r\n";
-                    
-                    // Check if the current criteria has been already rated by the user
-                    if (isset($_POST["criteria_".$criterion_nr]))
-                    {
-                      // This criteria has been already rated so check if the user selected the current rating
-                      if ($_POST["criteria_".$criterion_nr]==$rating_nr)
-                      {
-                        // it was selected so select it again (so the user doesn't lose there selection)
-                        echo '<input type="radio" id="criteria_'.$criterion_nr.'_'.$rating_nr.'" name="criteria_'.$criterion_nr.'" value = "'.$rating_nr.'" checked = "true" />'."\r\n";
-                      }
-                      else
-                      {
-                        // it wasnt selected so leave it unchecked
-                        echo '<input type="radio" id="criteria_'.$criterion_nr.'_'.$rating_nr.'" name="criteria_'.$criterion_nr.'" value = "'.$rating_nr.'" />'."\r\n";
-                      }
-                    }
-                    else
-                    {
-                      // The user has not yet rated this criteria so leave it unchecked
-                      echo '<input type="radio" id="criteria_'.$criterion_nr.'_'.$rating_nr.'" name="criteria_'.$criterion_nr.'" value = "'.$rating_nr.'" />'."\r\n";
-                    }
-                    
-                    // Create the label for the rating
-                    echo '<label class = "button" for="criteria_'.$criterion_nr.'_'.$rating_nr.'">'.$rating.'</label>'."\r\n";
-                    $rating_nr += 1;
-                  }
-                  echo '<br /><br />'."\r\n";
-                  $criterion_nr += 1;
-                }
-              ?>
-            </p>
-            <!--Other comments -->
-            <p class = "feedback_criteria">
-              Other comments: <br />
-              <?php
-                if(isset($_POST["other_comments"]))
+            //customer details section
+            echo '<h2>Customer Details</h2>'."\r\n";
+            echo '<p class= "customer_details">'."\r\n";
+            
+              //contact name
+              echo '<label class = "text" for = "contact_name">*Contact Name:</label>'."\r\n";
+              if (isset($_POST["contact_name"]))
+              {
+                echo '<input type = "text" id="contact_name" name="contact_name" value = "'.$_POST["contact_name"].'" />'."\r\n";
+              }
+              else
+              {
+                echo '<input type = "text" id="contact_name" name="contact_name" />'."\r\n";
+              }
+              echo '<br />'."\r\n";
+              
+              //company
+              echo '<label class = "text" for = "company">Company:</label>'."\r\n";
+              if (isset($_POST["company"]))
+              {
+                echo '<input type = "text" id="company" name="company" value = "'.$_POST["company"].'" />'."\r\n";
+              }
+              else
+              {
+                echo '<input type = "text" id="company" name="company" />'."\r\n";
+              }
+              echo '<br />'."\r\n";
+              
+              //email
+              echo '<label class = "text" for = "email">*Email:</label>'."\r\n";
+              if (isset($_POST["email"]))
+              {
+                echo '<input type = "text" id="email" name="email" value = "'.$_POST["email"].'" />'."\r\n";
+              }
+              else
+              {
+                echo '<input type = "text" id="email" name="email" />'."\r\n";
+              }
+            echo '</p>'."\r\n";
+            
+            //feedback section
+            echo '<h2>Feedback</h2>'."\r\n";
+            echo '<p class = "feedback_criteria">'."\r\n";
+
+            $criterion_nr = 1;
+            
+            foreach ($criteria as $criterion)
+            {
+              // output a comment e.g. <!-- Criterion 1 (Friendliness of staff) -->
+              echo '<!--Criterion '.$criterion_nr.' ('.$criterion.')-->'."\r\n";
+              
+              // output the actual criteria followed by a colon (:)
+              echo $criterion_nr.'.&nbsp;'.$criterion.': <br />'."\r\n";
+              
+              $rating_nr = 1;
+
+              foreach ($ratings as $rating)
+              {
+                // output a comment e.g. <!-- Rating 1 (N/A) -->
+                echo '<!--Rating '.$rating_nr.' ('.$rating.')-->'."\r\n";
+                
+                // check if the current criteria has been already rated by the user
+                if (isset($_POST["criteria_".$criterion_nr]))
                 {
-                  echo '<textarea id="other_comments" name="other_comments" rows="10" cols="70" >'.$_POST["other_comments"].'</textarea>'."\r\n";
+                  // this criteria has been already rated so check if the user selected the current rating
+                  if ($_POST["criteria_".$criterion_nr]==$rating_nr)
+                  {
+                    // it was selected so select it again (so the user doesn't lose there selection)
+                    echo '<input type="radio" id="criteria_'.$criterion_nr.'_'.$rating_nr.'" name="criteria_'.$criterion_nr.'" value = "'.$rating_nr.'" checked = "true" />'."\r\n";
+                  }
+                  else
+                  {
+                    // it wasnt selected so leave it unchecked
+                    echo '<input type="radio" id="criteria_'.$criterion_nr.'_'.$rating_nr.'" name="criteria_'.$criterion_nr.'" value = "'.$rating_nr.'" />'."\r\n";
+                  }
                 }
                 else
                 {
-                  echo '<textarea id="other_comments" name="other_comments" rows="10" cols="70" ></textarea>'."\r\n";
+                  // the user has not yet rated this criteria so leave it unchecked
+                  echo '<input type="radio" id="criteria_'.$criterion_nr.'_'.$rating_nr.'" name="criteria_'.$criterion_nr.'" value = "'.$rating_nr.'" />'."\r\n";
                 }
-              ?>
-            </p>
-            <input class = "submit" type = "submit" id = "submit_form" name = "submit_form" value= "Submit" />
-        </fieldset>
-      </form>
+                
+                // create the label for the rating
+                echo '<label class = "button" for="criteria_'.$criterion_nr.'_'.$rating_nr.'">'.$rating.'</label>'."\r\n";
+                $rating_nr += 1;
+              }
+              echo '<br /><br />'."\r\n";
+              $criterion_nr += 1;
+            }
+            echo '</p>'."\r\n";
+            
+            //other comments
+            echo '<!--other comments -->'."\r\n";
+            echo '<p class = "feedback_criteria">'."\r\n";
+              echo 'Other comments: <br />'."\r\n";
+              if(isset($_POST["other_comments"]))
+              {
+                echo '<textarea id="other_comments" name="other_comments" rows="10" cols="70" >'.$_POST["other_comments"].'</textarea>'."\r\n";
+              }
+              else
+              {
+                echo '<textarea id="other_comments" name="other_comments" rows="10" cols="70" ></textarea>'."\r\n";
+              }
+            echo '</p>'."\r\n";
+            //submit button
+            echo '<input class = "submit" type = "submit" id = "submit_form" name = "submit_form" value= "Submit" />'."\r\n";
+        echo '</fieldset>'."\r\n";
+      echo '</form>'."\r\n";
+    ?>
+    <!-- end dynamic content-->
     </div>
+    <!--footer section-->
     <div class="footer">
       <p>
         Webmaster: <a href="mailto:dwilkie@gmail.com?subject=MTS Website">David Wilkie</a>.
